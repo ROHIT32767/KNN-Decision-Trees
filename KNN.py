0adb2f52b956d9e86a2df4dfa2c5cd7e2b2acea1
ROHIT32767 <rohitgowlapalli01@gmail.com>
2023-08-26 18:55:46 +0530

    Pre-Final Submission

--- KNN.py
+++ KNN.py
@@ -4,6 +4,7 @@ import sys
 import time
 import numpy as np
 import pandas as pd
+from numpy import linalg as LA
 from numpy.linalg import norm
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
@@ -14,7 +15,6 @@ class Best_KNN:
         self.k = k
         self.encoder_type = encoder_type
         self.metric = metric
-        self.measure = 'mode'
         self.ratio = ratio
 
     def get_encoder_type(self):
@@ -38,21 +38,18 @@ class Best_KNN:
         self.ratio = ratio
 
     def get_measure(self, A):
-        if self.measure == 'mode':
-            unique_values, counts = np.unique(np.array(A), return_counts=True)
-            mode_index = np.argmax(counts)
-            mode = unique_values[mode_index]
-            return mode
+        unique_values, counts = np.unique(np.array(A), return_counts=True)
+        return unique_values[np.argmax(counts)]
 
     def get_distance(self, E1, E2):
         if self.metric == 'manhattan':
-            return norm(np.array(E1)-np.array(E2),axis=1,ord=1)
+            return LA.norm(np.array(E1)-np.array(E2),axis=1,ord=1) # distance (x1,x2) -> |x1-x2|
         elif self.metric == 'euclidean':
-            return norm(np.array(E1)-np.array(E2),axis=1,ord=2)
+            return LA.norm(np.array(E1)-np.array(E2),axis=1,ord=2)  # distance (x1,x2) -> |x1-x2|
         elif self.metric == 'cosine':
-            return 1-(np.dot(np.array(E1),np.array(E2))) / (norm(np.array(E1),axis=1) * norm(np.array(E2)))
+            return 1-(np.dot(np.array(E1),np.array(E2))) / (LA.norm(np.array(E1),axis=1) * LA.norm(np.array(E2)))
         else:
-            raise ValueError("Invalid metric")
+            raise ValueError("Unhandled Metric")
         
     def fit(self, train_embeddings, train_labels, validate_embeddings, validate_labels):
         self.train_embeddings = train_embeddings
@@ -63,64 +60,43 @@ class Best_KNN:
     def data_split(self,data):
         self.labels = np.array(data[:, 3])
         self.embeddings = np.array(data[:,1:3])
-        resnet = np.array(data[:,1])
-        self.resnet = [res[0] for res in resnet]
-        self.resnet = np.array(self.resnet)
-        vit = np.array(data[:,2])
-        self.vit = [v[0] for v in vit]
-        self.vit = np.array(self.vit)
+        self.resnet = np.array([res[0] for res in np.array(data[:,1])])
+        self.vit = np.array([v[0] for v in np.array(data[:,2])])
         num_total_samples = data.shape[0]
         num_training_samples = int(num_total_samples * self.ratio)
         indices = np.array(range(num_total_samples)) # used to check if unshuffled data is giving same results across users
-        np.random.shuffle(indices) # permutes the array [0,....n-1] 
+        np.random.seed(42)
+        indices = np.random.permutation(indices) # permutes the array [0,....n-1] 
         self.indices = indices # saving indices
         self.num_training_samples = num_training_samples # saving number of training samples
 
     def evaluate(self, embeddings, true_labels):
-        predicted_labels = self.predict_array(embeddings)
-        F1_score = f1_score(true_labels, predicted_labels, average='macro')
-        accuracy = accuracy_score(true_labels, predicted_labels)
-        precision = precision_score(true_labels, predicted_labels, average='macro',zero_division=0)
-        recall = recall_score(true_labels, predicted_labels, average='macro',zero_division=0)
-        return F1_score, accuracy, precision, recall
+        predicted_labels = self.predict(embeddings)
+        return f1_score(true_labels, predicted_labels, average='macro'),accuracy_score(true_labels, predicted_labels),precision_score(true_labels, predicted_labels, average='macro',zero_division=0), recall_score(true_labels, predicted_labels, average='macro',zero_division=0)
 
     def train(self, encoder_type):
         if encoder_type == 'vit':
             train_index = self.indices[:self.num_training_samples]
             validate_index = self.indices[self.num_training_samples:]
-            data_train = self.vit[train_index]
-            data_validate = self.vit[validate_index]
-            label_train = self.labels[train_index]
-            label_validate = self.labels[validate_index]
-            self.fit(data_train, label_train, data_validate, label_validate)
+            self.fit(self.vit[train_index],self.labels[train_index],self.vit[validate_index],self.labels[validate_index])
         elif encoder_type == 'resnet':
             train_index = self.indices[:self.num_training_samples]
             validate_index = self.indices[self.num_training_samples:]
-            data_train = self.resnet[train_index]
-            data_validate = self.resnet[validate_index]
-            label_train = self.labels[train_index]
-            label_validate = self.labels[validate_index]
-            self.fit(data_train, label_train, data_validate, label_validate)
+            self.fit(self.resnet[train_index],self.labels[train_index],self.resnet[validate_index],self.labels[validate_index])
 
     def predict_sample(self, E):
         distances = self.get_distance(self.train_embeddings, E)
         sorted_indices = np.argsort(distances)
-        nearest_index = sorted_indices[:self.k]
-        nearest_labels = self.train_labels[nearest_index]
-        classified_label = self.get_measure(nearest_labels)
-        return classified_label
+        return self.get_measure(self.train_labels[sorted_indices[:self.k]])
 
-    def predict_array(self, X):
-        predictions = [self.predict_sample(embeddings) for embeddings in X]
-        return np.array(predictions)
+    def predict(self, X):
+        return np.array([self.predict_sample(embeddings) for embeddings in X])
     
     def print_answer(self,data):
         self.data_split(data)
         self.train(self.encoder_type)  
         F1_score, accuracy, precision, recall = self.evaluate(self.validate_embeddings, self.validate_labels)
-        print_data = [['Accuracy',accuracy],['Precision',precision],['Recall',recall],['f1_score',F1_score]]
-        df = pd.DataFrame(print_data,columns=['Measure','Value'])
-        print(df.to_string(index=False))
+        print(pd.DataFrame([['Accuracy',accuracy],['Precision',precision],['Recall',recall],['f1_score',F1_score]],columns=['Measure','Value']).to_string(index=False))
     
 if len(sys.argv) != 2:
     print("Usage: python KNN.py <path_to_input_file>")
@@ -131,6 +107,24 @@ input_file = sys.argv[1]
 if not os.path.isfile(input_file):
     print("Error: Input file '{}' not found.".format(input_file))
     sys.exit(1)
-data = np.load(input_file, allow_pickle=True)
-Best_KNN_Object = Best_KNN(3,'vit','manhattan')
-Best_KNN_Object.print_answer(data)
+
+
+test_data = np.load(input_file, allow_pickle=True)
+train_data = np.load('data.npy',allow_pickle=True)
+Best_KNN_Object = Best_KNN(17,'vit','manhattan')
+# Best_KNN_Object = Best_KNN(3,'vit','euclidean')
+Best_KNN_Object.data_split(train_data)
+Best_KNN_Object.train(Best_KNN_Object.encoder_type)
+vit = np.array([v[0] for v in np.array(test_data[:,2])])
+resnet = np.array([res[0] for res in np.array(test_data[:,1])])
+if Best_KNN_Object.encoder_type == 'vit':
+    test_embeddings = vit
+elif Best_KNN_Object.encoder_type == 'resnet':
+    test_embeddings = resnet
+test_labels = np.array(test_data[:, 3])
+F1_score, accuracy, precision, recall = Best_KNN_Object.evaluate(test_embeddings,test_labels)
+print(pd.DataFrame([['Accuracy',accuracy],['Precision',precision],['Recall',recall],['f1_score',F1_score]],columns=['Measure','Value']).to_string(index=False))
+
+
+
+
